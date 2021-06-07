dd5656b7568b5a8ea63b1004f1abed5144c0d65b
Ryan Eballar <ryeballar@gmail.com>
2021-06-07 10:22:58 +0800

    removed unnecessary files

--- material/js/build-config.js
+++
@@ -1,7 +0,0 @@
-DocsApp.constant('BUILDCONFIG', {
-  "ngVersion": "1.3.2",
-  "version": "0.7.1",
-  "repository": "https://github.com/angular/material",
-  "date": "2015-02-02 10:28:44 -0800",
-  "commit": "f1a4bcc3d1cbd89b72f2eca120ffa6523f0f8c27"
-});
